53fe226cdaef6a29877b4407a84ca836ca058977
valeriaacoronaa <vcorona@mail.csuchico.edu>
2019-09-11 16:24:48 -0700

    allows user to enter numbers

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -4,12 +4,16 @@
 int main()
 {
   //declare variables here
-
+  int first = 0;
+  int second = 0;
+  
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
-
+  std::cin>>first;
+  
   std::cout<<"Please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
+  std:: cin>>second;
 
   //determine if first or second is bigger and store that in a third variable
 
