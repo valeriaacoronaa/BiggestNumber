2d65692af22f24c667af8af308130e393a513c8b
valeriaacoronaa <vcorona@mail.csuchico.edu>
2019-09-11 16:59:34 -0700

    determines which number is bigger

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -29,7 +29,7 @@ int main()
   std::cout<<"Of those two numbers, the biggest is: " << biggest << "\n";
   //print out the variable representing the biggest number
 
-  std::cout<<std::<<"Thank you for playing.\n";
+  std::cout<<"Thank you for playing.\n";
   
   return 0;
 }
